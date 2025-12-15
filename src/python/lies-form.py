a03e93b5880ef29492000c9992f204f898634729
pb-06 <kkandomail@gmail.com>
2025-12-15 08:41:50 +0100

    Create form

--- src/python/lies-form.py
+++ src/python/lies-form.py
@@ -7,7 +7,19 @@ with st.form(key="my-form"):
     col1, col2 = st.columns(2)
     with col1:
         st.subheader("Politician")
+        politician_name = st.text_input("Name", placeholder="Ide írd a nevet...")
+        age = st.number_input("Age", min_value=18, step=1)
     with col2:
         st.subheader("Party")
+        party_name = st.selectbox("Name", ["1", "2", "3", "4", "5"])
+        color = st.color_picker("Color")
+    
+    col3, col4 = st.columns(2)
+    with col3:
+        st.subheader("Lie")
+        lie = st.text_area("Lie")
+    with col4:
+        st.subheader("Review")
+        consent = st.checkbox(" Yes, I really want to store these data!")
 
     submit = st.form_submit_button()
